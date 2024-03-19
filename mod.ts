5a8e7cd2586d71d749e58aaca923d1c3059701aa
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2024-03-19 17:43:09 +0300

    Add exports

--- mod.ts
+++ mod.ts
@@ -1,2 +1,4 @@
 export { BoundMethod } from "./src/BoundMethod.ts";
+export { MemoizedGetter } from "./src/MemoizedGetter.ts";
 export { type ClassMethod } from "./src/ClassMethod.ts";
+export { type Nullable } from "./src/Nullable.ts";
